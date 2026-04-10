d746d477eed3dfb6f2da9e2eedc1752dce0000ec
alexia2010 <159458183+alexia2010@users.noreply.github.com>
2026-04-10 10:46:18 +0000

    tempo atual

--- main.js
+++ main.js
@@ -2,7 +2,6 @@ const botoes = document.querySelectorAll(".botao");
 const textos = document.querySelectorAll(".aba-conteudo");
 const contadores = document.querySelectorAll(".contador");
 
-contadores[0].textContent = "Contagem regressiva!";
 
 for(let i = 0; i < botoes.length; i = i + 1){
     botoes[i].onclick = function(){
@@ -13,6 +12,11 @@ for(let i = 0; i < botoes.length; i = i + 1){
         botoes[i].classList.add("ativo");
         textos[i].classList.add("ativo");
     }
-
+    
 }
+const tempoObjetivo01 = new Date("2026-10-05T00:00:00");
+let tempoAtual = new Date();
+contadores[0].textContent = tempoObjetivo - tempoAtual;
+
+
 console.log(botoes);
